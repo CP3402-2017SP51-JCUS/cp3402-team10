0d52c378c8a8824afcd07210ae0192f1780a400c
StenleyTok <stenleytok@gmail.com>
2017-05-08 21:14:16 +0800

    First commit
    Created custom theme with header and footer

---
+++ wp-content/themes/tk/header.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package _tk
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php do_action( 'before' ); ?>
+
+<header id="masthead" class="site-header" role="banner">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+		<div class="row navtop">
+			<div class="site-header-inner col-sm-12">
+
+				<?php $header_image = get_header_image();
+				if ( ! empty( $header_image ) ) { ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+					</a>
+				<?php } // end if ( ! empty( $header_image ) ) ?>
+
+
+				<div class="site-branding">
+					<h1 class="site-title alignleft"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<div class="alignright menu-title">Menu<span class="glyphicon glyphicon-menu-hamburger"></span></div>
+				</div>
+			</div>
+		</div>
+
+		<?php if(is_front_page()) {?>
+		<div class="row">
+			<h2 class="sans"><?php echo bloginfo( 'name' ); ?></h2>
+      <h3 class="sans"><?php echo bloginfo( 'description' ); ?></h3>
+			<div class="header-btn">
+				<ul>
+					<li><a class="jumbobutton" href="#">Video</a></li>
+					<li><a class="jumbobutton" href="#">See Our Work</a></li>
+		</div>
+		<?php } ?>
+</header><!-- #masthead -->
+
+
+
+<div class="main-content">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	<div class="container">
+		<div class="row">
+			<div id="content" class="main-content-inner col-sm-12 col-md-8">
